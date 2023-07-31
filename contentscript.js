954003fd5732ce4c0b22c7ef761c06a13757209f
adicarmel <adi.carmel@tomorrow.io>
2023-07-31 20:35:17 +0300

    add title text + add placeholder img

--- contentscript.js
+++ contentscript.js
@@ -8,17 +8,29 @@ const personNames = [
     'ביבי נתניהו',
     'בנימין נתניהו'
 ];
+const PLACEHOLDER_URL = 'https://placehold.co/600x400/EEE/31343C?font=oswald&text=image%20of%20a%20dictator'
 
 const images = document.getElementsByTagName('img');
+
+function changeImag({ image }) {
+    const newImage = new Image();
+    newImage.src = PLACEHOLDER_URL;
+    newImage.style = image.style;
+    image.parentNode.replaceChild(newImage, image);
+}
 
 Array.from(images).forEach((image) => {
     const altText = image.getAttribute('alt');
     const src = image.getAttribute('src');
+    const title = image.getAttribute('title');
+
     personNames.forEach((personName) => {
-        if (altText && altText.toLowerCase().includes(personName.toLowerCase())) {
-            image.style.display = 'none';
-        } else if (src && src.toLowerCase().includes(personName.toLowerCase())) {
-            image.style.display = 'none';
+        const inAltText = altText && altText.toLowerCase().includes(personName.toLowerCase());
+        const inSrc = src && src.toLowerCase().includes(personName.toLowerCase());
+        const inTitle = title && title.toLowerCase().includes(personName.toLowerCase());
+
+        if (inAltText || inSrc || inTitle) {
+            changeImag({ image });
         }
     });
 });
